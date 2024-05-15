a7d974ffefd6562a473ec0a1cc89ef0190254ae9
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-15 23:07:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
     //Write your code here
-  
+	 let radius = document.getElementById("radius").value;
+    let radius = parseFloat(radius);
+    if (!isNaN(radius) && radius >= 0) {
+        let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+        let roundedVolume = volume.toFixed(4);
+        document.getElementById("volume").value = roundedVolume;
+    } else {
+        document.getElementById("volume").value = 'NaN';
+    }
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
